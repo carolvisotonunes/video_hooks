c296b241cb35516ad39e57d5650d9edd86b1a65d
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-06-01 15:24:44 +1000

    removing a callback

--- src/components/App.js
+++ src/components/App.js
@@ -24,9 +24,6 @@ const App =() =>{
      setSelectedVideo(response.data.items[0]);
    };
 
-    const onVideoSelect = (video) => {
-        setSelectedVideo(video);
-    };
 
     return (
           <div className="ui container">
@@ -38,7 +35,7 @@ const App =() =>{
                 </div>
                 <div className="five wide column">
                   <VideoList
-                    onVideoSelect={onVideoSelect}
+                    onVideoSelect={setSelectedVideo}
                     videos={videos}
                   />
                 </div>
